3b6fa243fbb95bb65dc3f00f872fb0b2b3f20499
ziadbari <ziadbari01712@gmail.com>
2024-12-19 00:00:20 +0600

    Add files via upload

---
+++ positive negative determinate.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+                int num;
+                printf("Enter any number\n");
+                scanf("%d",&num);
+
+                if(num>0)
+                                printf("positive\n");
+                else if
+                (num<0)
+
+                printf("negative\n");
+
+                return 0;
+}
