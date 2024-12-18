c265f00b5d7a8c274ada30d13c81375fc56ec0fd
ziadbari <ziadbari01712@gmail.com>
2024-12-18 19:26:30 +0600

    Add files via upload

---
+++ data type 3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main(void)
+{
+                int num1,num2;
+
+                printf("Enter number 1\n");
+                printf("Enter number 2\n");
+                scanf("%d",&num1);
+                scanf("%d",&num2);
+
+
+
+
+
+                if(num1>num2)
+                                printf("Large=%d\n",num1);
+
+                                else if(num1<num2)
+                                                printf("Large=%d\n",num2);
+                                if (num1=num2)
+                                                printf("equal");
+
+                                return 0;
+
+
+}
