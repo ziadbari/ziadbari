efc371aafc94e9ab5edd06da4d9655a4b6f8e4c9
ziadbari <ziadbari01712@gmail.com>
2024-12-18 19:56:07 +0600

    Add files via upload

---
+++ Even odd derminate.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(void)
+{
+                int num;
+                printf("Enter any number:");
+                scanf("%d",&num);
+
+                if(num%2==0)
+                printf("even");
+
+
+                else if(num%2!=0)
+
+                printf("odd");
+
+
+                return 0;
+}
