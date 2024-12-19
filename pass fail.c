4a2c84f1627d3f4c6db005ff32259a839499013a
ziadbari <ziadbari01712@gmail.com>
2024-12-19 19:42:25 +0600

    Add files via upload

---
+++ pass fail.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void)
+{
+                int num;
+                printf("Enter any numbers: ");
+                scanf("%d",&num);
+
+                if(num>=33)
+                                printf("pass");
+                else
+                                printf("fail");
+
+                return 0;
+}
