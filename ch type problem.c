606d06372f31db77608f72bd69b205d1c2b78a87
ziadbari <ziadbari01712@gmail.com>
2024-12-19 11:48:09 +0600

    Add files via upload

---
+++ ch type problem.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+                char ch;
+                printf("Enter any letters\n");
+                scanf("%c",&ch);
+
+                if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+
+                printf("vowel");
+
+                else
+                                printf("consonant");
+
+                return 0;
+}
