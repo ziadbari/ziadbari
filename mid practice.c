a40688fb6a6be838651d5a8ebfc32279090f0008
ziadbari <ziadbari01712@gmail.com>
2024-12-21 18:40:48 +0600

    Add files via upload

---
+++ mid practice.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void)
+{
+                float radius,area;
+                printf("Enter the radius: ");
+                scanf("%f",&radius);
+                area=3.1416*radius*radius;
+
+                printf("Area of circle is %f\n",area);
+
+
+                return 0;
+}
