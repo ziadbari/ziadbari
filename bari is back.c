fa9b04e42cd0c5b0058a95e93271f3b9073fb10c
ziadbari <ziadbari01712@gmail.com>
2025-11-04 21:03:40 +0600

    Add files via upload

--- bari is back.c
+++ bari is back.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+int main()
+{
+int n,m;
+printf("Enter the size of the array: ") ;
+scanf("%d",&m);
+
+int a[n];
+printf("Enter the number of the element to take input (<=%d):",n);
+scanf("%d",&m);
+
+printf("Enter %d elements: \n",m);
+for(int i=0;i<m;i++)
+{
+scanf("%d",&a[i]);
+}
+printf("Array elements are: ");
+for(int i=0;i<m;i++)
+{
+printf("%d",a[i]);
+}
+return 0;
+
+}
 
