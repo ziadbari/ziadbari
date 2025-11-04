966fd894685eb1ab32a008a2c346767c5773a61c
ziadbari <ziadbari01712@gmail.com>
2025-11-04 18:01:11 +0600

    Add files via upload

---
+++ ziad.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+int a[10]={1,2,3,4,5,6,7,8,9,10};
+
+
+printf("%d",a[0]);
+printf("%d",a[1]);
+printf("%d",a[2]);
+printf("%d",a[3]);
+printf("%d",a[4]);
+printf("%d",a[5]);
+printf("%d",a[6]);
+printf("%d",a[7]);
+printf("%d",a[8]);
+printf("%d",a[9]);
+return 0;
+
+}
